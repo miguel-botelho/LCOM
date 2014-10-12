1b27bb81a554006109e9ac1d9ad8acf4176b14cb
up201303568 <up201303568@19603280-5e82-4f85-9194-10ac69b20044>
2014-10-12 12:18:28 +0000

    only interrupts missing

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,49 +1,93 @@
+#include <minix/drivers.h>
+
 #include"i8254.h"
 #include"timer.h"
 
-int main(int argc, char *argv[])
-{
-	/* Initialize service */
-	sef_startup();
+static int proc_args(int argc, char *argv[]);
+static unsigned long parse_ulong(char *str, int base);
+static void print_usage(char *argv[]);
 
-	if (argc == 1)
-	{
-	timer_set_square(TIMER_0, 30);
-	printf("\tTimer 0 \n");
-	timer_test_config(TIMER_0);
+int main(int argc, char **argv) {
 
-	printf("\tTimer 1 \n");
-	timer_test_config(TIMER_1);
+  /* Initialize service */
 
-	printf("\tTimer 2 \n");
-	timer_test_config(TIMER_2);
+  sef_startup();
 
-	printf("To display a selected timer just enter a number between [0..2]\n\n");
-	}
-	else
-		if ((strncmp(argv[1], "0", strlen("0")) == 0))
-		{
-			printf("\tTimer 0 \n");
-			timer_test_config(TIMER_0);
-		}
-		else
-			if ((strncmp(argv[1], "1", strlen("1")) == 0))
-			{
-				printf("\tTimer 1 \n");
-				timer_test_config(TIMER_1);
-			}
-			else
-				if ((strncmp(argv[1], "2", strlen("2")) == 0))
-				{
-					printf("\tTimer 2 \n");
-					timer_test_config(TIMER_2);
-				}
-				else
-					{
-					printf("Enter a valid number! %u\n\n", *argv[1]);
-					return 1;
-					}
+  printf("lab2: TIMERS \n");
 
+  if ( argc == 1 ) {
+      print_usage(argv);
+      return 0;
+  } else {
+      proc_args(argc, argv);
+  }
+  return 0;
 
-	return 0;
+}
+
+static void print_usage(char *argv[]) {
+  printf("Usage: one of the following:\n"
+	 "\t service run %s -args \"test_square <freq>\" \n"
+	 "\t service run %s -args \"test_int <time>\" \n"
+	 "\t service run %s -args \"test_config <timer>\" \n",
+	 argv[0], argv[0], argv[0]);
+}
+
+static int proc_args(int argc, char *argv[]) {
+
+  unsigned long timer, freq, time;
+
+  /* check the function to test: if the first characters match, accept it */
+  if (strncmp(argv[1], "test_square", strlen("test_square")) == 0) {
+	  if( argc != 3 ) {
+		  printf("timer: wrong no of arguments for test of timer_test_square() \n");
+		  return 1;
+	  }
+	  if((freq = parse_ulong(argv[2], 10)) == ULONG_MAX)
+		  return 1;
+	  printf("timer:: timer_test_square(%u)\n\n", freq); /* Actually, it was already invoked */
+	  return timer_test_square(freq);
+  } else if (strncmp(argv[1], "test_int", strlen("test_int")) == 0) {
+	  if( argc != 3 ) {
+		  printf("timer: wrong no of arguments for test of timer_test_int() \n");
+		  return 1;
+	  }
+	  if((time = parse_ulong(argv[2], 10)) == ULONG_MAX)
+	  		  return 1;
+	  printf("timer:: timer_test_int(%u)\n\n", time);
+	  return timer_test_int(time);
+  } else if (strncmp(argv[1], "test_config", strlen("test_config")) == 0) {
+	  if( argc != 3 ) {
+		  printf("timer: wrong no of arguments for test of timer_test_config() \n");
+		  return 1;
+	  }
+	  if((timer = parse_ulong(argv[2], 10)) == ULONG_MAX)
+	  	  		  return 1;
+	  printf("timer:: timer_test_config(%u)\n\n", timer);
+	  return timer_test_config(timer);
+  } else {
+	  printf("timer: non valid function \"%s\" to test\n", argv[1]);
+	  return 1;
+  }
+}
+
+static unsigned long parse_ulong(char *str, int base) {
+  char *endptr;
+  unsigned long val;
+
+  val = strtoul(str, &endptr, base);
+
+  if ((errno == ERANGE && val == ULONG_MAX )
+	  || (errno != 0 && val == 0)) {
+	  perror("strtol");
+	  return ULONG_MAX;
+  }
+
+  if (endptr == str) {
+	  printf("video_txt: parse_ulong: no digits were found in %s \n", str);//temos de mudar isto
+	  return ULONG_MAX;
+  }
+
+  /* Successful conversion */
+  return val;
 }
