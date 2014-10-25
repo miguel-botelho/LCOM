bcf8abbce1d37f73a65d578a661ff39288b954a9
Miguel Botelho <m_botelho8@hotmail.com>
2014-10-25 11:25:55 +0000

    timed_scan feita!!! :D

--- lab3/test3.c
+++ lab3/test3.c
@@ -176,5 +176,101 @@ int kbd_test_leds(unsigned short n, unsigned short *leds) {
 }
 
 int kbd_test_timed_scan(unsigned short n) {
-	/* To be completed */
+		int r;
+		int key = 0;
+		int return_byte;
+		int ipc_status;
+		int msb;
+		unsigned short counter = 0;
+		msb = 0;
+		message msg;
+		unsigned long freq = 60;
+		int temp_counter = 0;
+		char irq_set = BIT(khook_id);
+		char irq_set_timer = BIT(hook_id);
+		timer_test_square(freq); //garante que o timer 0 está "certo"
+
+		if (-1 == timer_subscribe_int())
+		{
+			printf("Fail to subscribe Timer 0!\n\n");
+			return 1;
+		}
+
+		if (-1 == kbd_subscribe_int())
+		{
+			printf("Fail to subscribe Keyboard!\n\n");
+			return 1;
+		}
+		while((KBD_ESC_KEY != key) && (counter < n) ) { /* When ESC key is pressed */
+			/* Get a request message. */
+			if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+				printf("driver_receive failed with: %d", r);
+				continue;
+			}
+			if (is_ipc_notify(ipc_status)) { /* received notification */
+				switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: /* hardware interrupt notification */
+					if (msg.NOTIFY_ARG & irq_set_timer)
+					{
+						temp_counter++;
+						if ((temp_counter/60) == 1) //se for true quer dizer que passou um segundo
+							{
+								temp_counter = 0; //reset do temp_counter para voltar a contar os primeiros freq tiques
+								counter++;
+							}
+					}
+					if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+						counter = 0;
+						if (return_byte != 1) //if return_byte == 1 then the key has 2 bytes
+						{
+							return_byte = kbd_scan_c(&key);
+							if (return_byte == 0)
+							{
+								if (0x0080 == (0x0080 & key))
+								{
+									printf("Breakcode: 0x%x\n", key);
+								}
+								else
+								{
+									//if (key == 0)
+									{
+										printf("Makecode: 0x%x\n", key);
+									}
+								}
+							}
+						}
+						else
+						{
+							msb = key;
+							msb = msb << 8;
+							return_byte = kbd_scan_c(&key);
+							key = key & 0x00FF;
+							key = key + msb;
+							if (0x0080 == (0x0080 & key))
+							{
+								printf("Breakcode: 0x%x\n", key);
+							}
+							else
+							{
+								//if (key == 0)
+								{
+									printf("Makecode: 0x%x\n", key);
+								}
+							}
+							//return_byte = 0;
+							//kbd_scan_c(&key);
+						}
+						/* process it */
+					}
+					break;
+				default:
+					break; /* no other notifications expected: do nothing */
+				}
+			} else { /* received a standard message, not a notification */
+				/* no standard messages expected: do nothing */
+			}
+		}
+		timer_unsubscribe_int();
+		kbd_unsubscribe_int();
+		return 0;
 }
