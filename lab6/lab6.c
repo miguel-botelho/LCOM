8123766109cc2d8160b52c7193870d991fec6a45
Miguel Botelho <m_botelho8@hotmail.com>
2014-12-01 15:22:22 +0000

    Makefile adicionado e date provavelmente feita

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,4 +1,24 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <machine/int86.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+#include "test6.h"
+
 int main(int argc, char **argv) {
+
+
+	 /* Initialize service */
+
+	  sef_startup();
+
+	  /* Enable IO-sensitive operations for ourselves */
+
+	  sys_enable_iop(SELF);
+
+
+	  return 0;
 
 	/* To be completed */
 
--- lab6/test6.h
+++ lab6/test6.h
@@ -1,3 +1,18 @@
+
+
+#define RTC_IRQ 8
+#define RTC_ADDR_REG 0x70
+#define RTC_DATA_REG 0x71
+
+#define BIT(n) 		(0x01<<(n))
+
+extern int rtc_hook_id;
+
+
+unsigned long rtc_subscribe_int(void);
+int RTC_unsubscribe_int();
+short read_rtc(long array);
+
 int rtc_test_conf(void);
 int rtc_test_date(void);
 int rtc_test_int(/* to be defined in class */);
