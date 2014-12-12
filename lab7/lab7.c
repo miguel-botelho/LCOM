2f0a14d5be79b6df0265ee496ad3d170a4b0a23c
António Ramadas <antonio-ramadas@users.noreply.github.com>
2014-12-12 14:42:27 +0000

    Import of code provided for Lab 7

---
+++ lab7/lab7.c
@@ -0,0 +1,14 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <machine/int86.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+#include"test7.h"
+
+int main(int argc, char **argv) {
+
+	/* To be completed */
+
+}
+
