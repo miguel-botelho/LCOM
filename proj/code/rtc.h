67678a960927fb33806d134ba543c05d89974d72
António Ramadas <antonio-ramadas@users.noreply.github.com>
2014-12-13 11:16:14 +0000

    Makefile improved

---
+++ proj/code/rtc.h
@@ -0,0 +1,8 @@
+#ifndef __RTC_H
+#define __RTC_H
+
+#include "rtc_macros.h"
+
+
+
+#endif /* __RTC_H */
---
+++ proj/code/rtc_macros.h
@@ -0,0 +1,6 @@
+#ifndef __RTC_MACROS_H
+#define __RTC_MACROS_H
+
+
+
+#endif /* __RTC_MACROS_H */
