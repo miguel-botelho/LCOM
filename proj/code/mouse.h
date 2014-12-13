a33daf880351c62fba13778714b064299339b0b7
up201303568 <up201303568@19603280-5e82-4f85-9194-10ac69b20044>
2014-12-13 16:51:22 +0000

    Mouse now works!!

--- proj/code/mouse.h
+++ proj/code/mouse.h
@@ -7,22 +7,15 @@
 #include "mouse_struct.h"
 #include "keyboard_mouse_macros.h"
 
-/**
- * @brief Recept the packets via interrupts
- *
- * Fill the struct with the packets received from the mouse
- * Exits after receiving the number of packets.
- *
- * @param hi the mouse id (required to read the 3-row bytes)
- *
- * @return Return 0 upon success and non-zero otherwise
- */
-int test_packet(); //hi = hook id
 
 /**
  * @brief Fill the mouse struct
  *
  * Fill the mouse struct.
+ *
+ * @param *a pointer to the array to fill the mouse struct
+ *
+ * @return void
  *
  */
 void fill_struct(char *a);
@@ -32,13 +25,50 @@ void fill_struct(char *a);
  *
  * Interprets a 8 bits negative number. Returns it in the correct way (2 complement)
  *
+ * @param neg Number to be interpreted as negative
+ *
  * @return Return 0 upon success and non-zero otherwise
  */
 char neg8bits(char neg);
 
+/**
+ * @brief Take cares of the process of sending a command
+ *
+ * Call different functions to send the cmd
+ *
+ * @param cmd command to be sent
+ *
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int mouse_int_handler(unsigned long cmd);
+
+/**
+ * @brief Send command
+ *
+ * Send the argument to the respetive port
+ *
+ * @param cmd command to be sent
+ *
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int mouse_send_command(unsigned long cmd);
+
+/**
+ * @brief Send first command
+ *
+ * Send the first command before the actual one (see mouse_send_command() followed by mouse_int_handler())
+ *
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int mouse_send_first_command();
+
+/**
+ * @brief Receives the consequence of the command sent
+ *
+ * Receive the command of the mouse
+ *
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int mouse_cmd_receive();
 
 #endif /* __MOUSE_H */
--- proj/code/proj.c
+++ proj/code/proj.c
@@ -2,19 +2,13 @@
 #include "lib.h"
 
 #include "subscribe_macros.h"
-
-//#include "hook_id.h"
-
 #include "device_interrupts.h"
 #include "keyboard.h"
 #include "mouse.h"
 #include "timer.h"
 #include "video_gr.h"
 #include "bitmap.h"
-
-// NEEDS TO BE MADE!!!!!!!
-//bitmap.c:113 -> start address of the video card (to draw)
-
+#include "rtc.h"
 
 int main(int argc, char **argv) {
 
@@ -30,6 +24,9 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
+	mouse_int_handler(SET_STREAM); //define o rato como stream mode
+	mouse_int_handler(ESDP); //ativa o envio dos dados por parte do rato
+
 	int contador = 0;
 	int temp_counter = 0;
 	int key = 0;
@@ -37,17 +34,28 @@ int main(int argc, char **argv) {
 	int ipc_status;
 	message msg;
 
-	vg_init(GRAPHIC_MODE_16_BITS);
-	Bitmap* bmp;
-
-	bmp = loadBitmap("home/lcom/proj/code/images/A.bmp");
-	drawBitmap(bmp, 0, 0 , ALIGN_LEFT);
+	// mouse
+	char bool1 = 0;
+	char bool2 = 0;
+	char a[3];
+	char byte1;
+	char byte2;
+	char byte3;
+	unsigned int mouse;
+	unsigned long key_register;
 
 	char irq_set_keyboard = BIT(macro_hook_id_keyboard);
 	char irq_set_timer = BIT(macro_hook_id_timer);
+	char irq_set_mouse = BIT(macro_hook_id_mouse);
+
+	/*vg_init(GRAPHIC_MODE_16_BITS);
+	Bitmap* bmp;
+
+	bmp = loadBitmap("home/lcom/proj/code/images/A.bmp");
+	drawBitmap(bmp, 0, 0 , ALIGN_LEFT);*/
 
 
-	while( (contador < 5) && (key != KBD_ESC_KEY) ) { /* You may want to use a different condition */
+	while( (key != KBD_ESC_KEY) ) {
 		/* Get a request message. */
 		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
 			printf("driver_receive failed with: %d", r);
@@ -63,32 +71,76 @@ int main(int argc, char **argv) {
 						contador++;
 						temp_counter = 0; //reset do temp_counter para voltar a contar os primeiros freq tiques
 					}
-					/* process it */
 				}
+
 				if (msg.NOTIFY_ARG & irq_set_keyboard)
 				{
 					kbd_scan_c(&key);
 				}
-				break;
+
+				if (msg.NOTIFY_ARG & irq_set_mouse)
+				{
+					sys_inb(OUT_BUF, &key_register);
+					mouse = (unsigned int) key_register;
+
+					//le o packet
+					if (bool1 == 0)
+					{
+						if (BIT(3) == (BIT(3) & mouse))
+						{
+							bool1 = 1; //o primeiro byte
+							byte1 = mouse;
+						}
+						else continue;
+
+					}
+					else
+					{
+						if (bool2 == 0)
+						{
+							bool2 = 1; //o segundo byte
+							byte2 = mouse;
+						}
+						else
+						{
+
+							//este e o 3 byte
+							//estao todos lidos
+							//bool3 = 1;
+							byte3 = mouse;
+							bool1 = 0;
+							bool2 = 0;
+
+							//os bytes passam para um array
+							a[0] = byte1;
+							a[1] = byte2;
+							a[2] = byte3;
+							fill_struct(a);
+							printf("Mouse: %d\n", mouse_t.x);
+						}
+					}
+				}
+			break;
 			default:
 				break; /* no other notifications expected: do nothing */
-			}
-		} else { /* received a standard message, not a notification */
-			/* no standard messages expected: do nothing */
 		}
+	} else { /* received a standard message, not a notification */
+		/* no standard messages expected: do nothing */
 	}
-	deleteBitmap(bmp);
-	vg_exit();
+}
+/*deleteBitmap(bmp);
+	vg_exit();*/
 
-	//code
-	//implementation
-	//...
-	if (unsubscribe_all() == -1)
-	{
-		printf("Failure to unsubscribe!! \n\n");
-		return -1;
-	}
+//estas duas opearacoes sao feitas para assegurar o normal funcionamento do rato quando acabar a funcao
+mouse_int_handler(DISABLE_STREAM); //desativa a stream
+mouse_int_handler(SET_STREAM); //volta a ativar a stream, isto foi feito para desativar o envio dos pacotes
 
-	return 0;
+if (unsubscribe_all() == -1)
+{
+	printf("Failure to unsubscribe!! \n\n");
+	return -1;
+}
+
+return 0;
 }
 
