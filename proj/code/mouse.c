f1d7c7d8419eff594c999a59607e4f1ac6d5f956
Miguel Botelho <m_botelho8@hotmail.com>
2014-12-13 14:52:43 +0000

    Testa o mouse antonio, no meu pc ta bugado

--- proj/code/mouse.c
+++ proj/code/mouse.c
@@ -123,3 +123,120 @@ void fill_struct(char *a) // the size of this array is fixed, so does not need t
 		mouse_t.y = a[2];
 	}
 }
+
+
+int mouse_int_handler(unsigned long cmd){
+	int cmd_receive = 0;
+	int cmd_receive2 = 0;
+	int nr_tentativas = 0;
+
+
+	while(1)
+	{
+		while(nr_tentativas < 5)
+		{
+			mouse_send_first_command();
+
+			/*cmd_receive = mouse_cmd_receive();
+
+			printf("done\n");
+
+			if(MOUSE_DATA == (MOUSE_DATA & cmd_receive))
+			{
+				if (NACK == cmd_receive || ERROR == cmd_receive)
+				{
+					printf("cmd1\n");
+					nr_tentativas++;
+					continue;
+				}
+			} else continue;
+			 */
+			break;
+		}
+
+		nr_tentativas = 0;
+
+		while (nr_tentativas < 5)
+		{
+			mouse_send_command(cmd);
+
+			cmd_receive2 = mouse_cmd_receive();
+
+			if(MOUSE_DATA == (MOUSE_DATA & cmd_receive2)) //verificar se o que estou a ler é do mouse
+			{
+				if (cmd_receive2 == NACK) // se e NACK, então repete só o cmd enviado, não necessita de enviar 0xD4
+				{
+					nr_tentativas++;
+					continue;
+				}
+				else break;
+			}
+			else continue;
+		}
+
+		if (cmd_receive2 == ERROR) //erro, volta a repetir todo o processo
+		{
+			continue;
+		}
+
+		break;
+	}
+
+	return 0;
+}
+
+int mouse_send_command(unsigned long cmd){
+
+	unsigned long stat;
+
+	while(1)
+	{
+		sys_inb(STAT_REG, &stat); /*assuming it returns OK*/
+		/*loop while 8042 input buffer is not empty*/
+		if ((stat & IBF) == 0)
+		{
+			sys_outb(COMMAND_PORT, cmd);
+			return 0;
+		}
+		tickdelay(micros_to_ticks(DELAY_US));
+	}
+
+}
+
+int mouse_send_first_command(){
+
+	unsigned long stat;
+
+
+	while(1)
+	{
+		sys_inb(STAT_REG, &stat); /*assuming it returns OK*/
+		/*loop while 8042 input buffer is not empty*/
+		if ((stat & IBF) == 0)
+		{
+			sys_outb(STAT_REG, MC);
+			return 0;
+		}
+		tickdelay(micros_to_ticks(DELAY_US));
+	}
+}
+
+int mouse_cmd_receive(){
+	unsigned long stat = 0;
+	unsigned long data = 0;
+
+	while(1)
+	{
+		sys_inb(STAT_REG, &stat); /*assuming it returns OK*/
+		/*loop while 8042 input buffer is not empty*/
+		if (stat & OBF)
+		{
+			sys_inb(OUT_BUF, &data); /*assuming it returns OK*/
+			if ((stat & (PAR_ERR | TO_ERR)) == 0)
+				return data;
+			else
+				return -1;
+		}
+		tickdelay(micros_to_ticks(DELAY_US));
+	}
+}
--- proj/code/proj.c
+++ proj/code/proj.c
@@ -11,6 +11,7 @@
 #include "timer.h"
 #include "video_gr.h"
 #include "bitmap.h"
+#include "mouse_struct.h"
 
 // NEEDS TO BE MADE!!!!!!!
 //bitmap.c:113 -> start address of the video card (to draw)
@@ -19,10 +20,45 @@
 int main(int argc, char **argv) {
 
 	// Initialize service
+
 	sef_startup();
 
 	// Enable IO-sensitive operations for ourselves
 	sys_enable_iop(SELF);
+
+
+
+	int r;
+	int ipc_status;
+	message msg;
+	int key = 0;
+	int temp_counter = 0;
+	int counter = 0;
+	char irq_set_keyboard = BIT(macro_hook_id_keyboard);
+	char irq_set_timer = BIT(macro_hook_id_timer);
+	char irq_set_mouse = BIT(macro_hook_id_mouse);
+
+	if (timer_set_square(0, 60) == -1)
+	{
+		printf("Time not set!\n");
+		return -1;
+	}
+
+	char a[3];
+	char bool2 = 0; //0 = true, nao 0 = true
+	char byte1;
+	char byte2;
+	char byte3;
+	unsigned int mouse;
+	unsigned long key_register;
+
+	//read the first byte
+	sys_inb(OUT_BUF, &key_register);
+	mouse = (unsigned int) key_register;
+	if (BIT(3) == (BIT(3) & mouse))
+	{
+		byte1 = mouse;
+	}
 
 	if (subscribe_all() == -1)
 	{
@@ -30,6 +66,11 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
+	mouse_int_handler(SET_STREAM); //define o rato como stream mode
+	printf("OLA");
+	mouse_int_handler(ESDP); //ativa o envio dos dados por parte do rato
+
+	printf("OLA");
 	vg_init(GRAPHIC_MODE_16_BITS);
 	Bitmap* bmp;
 	int contador = 0;
@@ -47,13 +88,67 @@ int main(int argc, char **argv) {
 			y = 0;
 		}
 	}
-	sleep(5);
+
+	while ( (key != KBD_ESC_KEY) && (counter < 5) && (mouse_t.LB == 0) )  // condição de saída
+	{
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				if (msg.NOTIFY_ARG & irq_set_keyboard) { /* subscribed interrupt */
+					kbd_scan_c(&key);
+				}
+				if (msg.NOTIFY_ARG & irq_set_timer)
+				{
+					temp_counter++;
+					if ((temp_counter/60) == 1) //se for true quer dizer que passou um segundo
+					{
+						temp_counter = 0;
+						counter++;
+					}
+				}
+
+				if (msg.NOTIFY_ARG & irq_set_mouse)
+				{
+					printf("NOTIFY_MOUSE\n");
+					sys_inb(OUT_BUF, &key_register);
+					mouse = (unsigned int) key_register;
+
+					//le o packet
+					if (bool2 == 0)
+					{
+						bool2 = 1; //o segundo byte
+						byte2 = mouse;
+					}
+					else
+					{
+						//este e o 3 byte
+						//estao todos lidos
+						//bool3 = 1;
+						byte3 = mouse;
+
+						//os bytes passam para um array
+						a[0] = byte1;
+						a[1] = byte2;
+						a[2] = byte3;
+						fill_struct(a); //fill the struct
+						printf("left button: %x\n", mouse_t.LB);
+					}
+				}
+			}
+		}
+	}
+
 	deleteBitmap(bmp);
 	vg_exit();
 
 	//code
 	//implementation
 	//...
+
 	if (unsubscribe_all() == -1)
 	{
 		printf("Failure to unsubscribe!! \n\n");
