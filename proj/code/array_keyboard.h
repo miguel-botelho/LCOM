80b616919be0355d1f7e9c67d8c5cfb815c95cd0
up201304828 <up201304828@19603280-5e82-4f85-9194-10ac69b20044>
2014-12-20 13:15:30 +0000

    funcoes adicionadas, menu mais limpo

--- proj/code/array_keyboard.h
+++ proj/code/array_keyboard.h
@@ -3,35 +3,6 @@
 
 #include "bitmap.h"
 
-Bitmap *key_scancode[86];
-
-key_scancode[0x0010] = loadBitmap("home/lcom/proj/code/images/font/word/Q.bmp");
-key_scancode[0x0011] = loadBitmap("home/lcom/proj/code/images/font/word/W.bmp");
-key_scancode[0x0012] = loadBitmap("home/lcom/proj/code/images/font/word/E.bmp");
-key_scancode[0x0013] = loadBitmap("home/lcom/proj/code/images/font/word/R.bmp");
-key_scancode[0x0014] = loadBitmap("home/lcom/proj/code/images/font/word/T.bmp");
-key_scancode[0x0015] = loadBitmap("home/lcom/proj/code/images/font/word/Y.bmp");
-key_scancode[0x0016] = loadBitmap("home/lcom/proj/code/images/font/word/U.bmp");
-key_scancode[0x0017] = loadBitmap("home/lcom/proj/code/images/font/word/I.bmp");
-key_scancode[0x0018] = loadBitmap("home/lcom/proj/code/images/font/word/O.bmp");
-key_scancode[0x0019] = loadBitmap("home/lcom/proj/code/images/font/word/P.bmp");
-
-key_scancode[0x001E] = loadBitmap("home/lcom/proj/code/images/font/word/A.bmp");
-key_scancode[0x001F] = loadBitmap("home/lcom/proj/code/images/font/word/S.bmp");
-key_scancode[0x0020] = loadBitmap("home/lcom/proj/code/images/font/word/D.bmp");
-key_scancode[0x0021] = loadBitmap("home/lcom/proj/code/images/font/word/F.bmp");
-key_scancode[0x0022] = loadBitmap("home/lcom/proj/code/images/font/word/G.bmp");
-key_scancode[0x0023] = loadBitmap("home/lcom/proj/code/images/font/word/H.bmp");
-key_scancode[0x0024] = loadBitmap("home/lcom/proj/code/images/font/word/J.bmp");
-key_scancode[0x0025] = loadBitmap("home/lcom/proj/code/images/font/word/K.bmp");
-key_scancode[0x0026] = loadBitmap("home/lcom/proj/code/images/font/word/L.bmp");
-
-key_scancode[0x002C] = loadBitmap("home/lcom/proj/code/images/font/word/Z.bmp");
-key_scancode[0x002D] = loadBitmap("home/lcom/proj/code/images/font/word/X.bmp");
-key_scancode[0x002E] = loadBitmap("home/lcom/proj/code/images/font/word/C.bmp");
-key_scancode[0x002F] = loadBitmap("home/lcom/proj/code/images/font/word/V.bmp");
-key_scancode[0x0030] = loadBitmap("home/lcom/proj/code/images/font/word/B.bmp");
-key_scancode[0x0031] = loadBitmap("home/lcom/proj/code/images/font/word/N.bmp");
-key_scancode[0x0032] = loadBitmap("home/lcom/proj/code/images/font/word/M.bmp");
+void fill_key_scancode(Bitmap ** scan_code);
 
 #endif
--- proj/code/menu.c
+++ proj/code/menu.c
@@ -11,3 +11,124 @@ void bitmaps_load(bitmap_struct *t)
 	t->lost = loadBitmap("home/lcom/proj/code/images/Lost.bmp");
 	t->win = loadBitmap("home/lcom/proj/code/images/Win.bmp");
 }
+
+void exit_menu(bitmap_struct bitmaps)
+{
+	deleteBitmap(bitmaps.background);
+	deleteBitmap(bitmaps.mouse);
+	deleteBitmap(bitmaps.frame);
+	vg_exit();
+
+	//estas duas opearacoes sao feitas para assegurar o normal funcionamento do rato quando acabar a funcao
+	mouse_int_handler(DISABLE_STREAM); //desativa a stream
+	mouse_int_handler(SET_STREAM); //volta a ativar a stream, isto foi feito para desativar o envio dos pacotes
+
+	if (unsubscribe_all() == -1)
+	{
+		printf("Failure to unsubscribe!! \n\n");
+	}
+}
+
+char position_menu(bitmap_struct bitmaps, char * video_copy)
+{
+	if ((mouse_t.x_mouse >= 438) && (mouse_t.x_mouse <= 591))
+	{
+		if ((mouse_t.y_mouse >= 650) && (mouse_t.y_mouse <= 716))
+		{
+			if (mouse_t.LB == 1)
+			{
+				exit_menu(bitmaps);
+				return 0;
+			}
+			else
+			{
+				change_color(438, 591, 650, 716, rgb(0,0,0), rgb(100, 200, 0), video_copy);
+			}
+		}
+	}
+
+	if ((mouse_t.x_mouse >= 305) && (mouse_t.x_mouse <= 718))
+	{
+		if ((mouse_t.y_mouse >= 555) && (mouse_t.y_mouse <= 644))
+		{
+			if (mouse_t.LB == 1)
+			{
+				exit_menu(bitmaps);
+				return 0;
+			}
+			else
+			{
+
+			}
+		}
+	}
+
+	if ((mouse_t.x_mouse >= 341) && (mouse_t.x_mouse <= 633))
+	{
+		if ((mouse_t.y_mouse >= 484) && (mouse_t.y_mouse <= 550))
+		{
+			if (mouse_t.LB == 1)
+			{
+				exit_menu(bitmaps);
+				return 0;
+			}
+			else
+			{
+
+			}
+		}
+	}
+
+	if ((mouse_t.x_mouse >= 248) && (mouse_t.x_mouse <= 787))
+	{
+		if ((mouse_t.y_mouse >= 412) && (mouse_t.y_mouse <= 466))
+		{
+			if (mouse_t.LB == 1)
+			{
+				exit_menu(bitmaps);
+				return 0;
+			}
+			else
+			{
+
+			}
+		}
+	}
+
+	if ((mouse_t.x_mouse >= 149) && (mouse_t.x_mouse <= 871))
+	{
+		if ((mouse_t.y_mouse >= 328) && (mouse_t.y_mouse <= 396))
+		{
+			if (mouse_t.LB == 1)
+			{
+				exit_menu(bitmaps);
+				return 0;
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
+
+int change_color(unsigned xi, unsigned xf, unsigned yi, unsigned yf, int color_init, int color_final, char * video_copy)
+{
+	video_copy = video_copy + (1024 * yi) + xi;
+	unsigned int i = xi;
+	unsigned int j = yi;
+	for (j; j < xf; j++)
+	{
+		for (i; i < yf; i++)
+		{
+			if (color_init == *video_copy)
+				*video_copy = color_final;
+			video_copy++;
+		}
+		i = 0;
+		video_copy = video_copy + 1024 - (xf - xi);
+	}
+	video_copy = getVideoMem();
+	return 0;
+}
+
